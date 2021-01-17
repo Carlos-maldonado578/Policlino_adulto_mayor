aa05b36586db5ba2752784e1f51003f25ca4f895
edv862 <evalderrama862@gmail.com>
2021-01-17 16:55:41 -0300

    Deploy en heroku

--- policlinicas/app.js
+++ policlinicas/app.js
@@ -5,10 +5,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const port = process.env.PORT || 3000;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.listen(port, function(){
+  console.log("Server started successfully...");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
